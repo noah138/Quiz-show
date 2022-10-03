36d944085c5e75dbcc6dee5f796a5d08ec6fd59c
noah138 <noahschwartz@live.com>
2022-10-02 18:18:00 -0700

    Added hover effects to starting buttons and added some simple pseudo code

---
+++ script.js
@@ -0,0 +1,6 @@
+// Answers are stored in an arary, with variable correct answer correlating to the position of the correct answer in the array
+// When play is pressed, take to first question and timer is started
+// If right answer is selected, add point/any other alerts (answer turns green) and go to next question
+// If wrong answer is selected, deduct time from timer, do not add point, and go to next question
+// Repeat this until question number equals the total length of the questions
+// Report a score and store it in the leaderboards
