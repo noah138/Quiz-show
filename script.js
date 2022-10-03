974dba9def08cc6b84e42c1f5cd6113b38a8815b
noah138 <noahschwartz@live.com>
2022-10-02 20:12:14 -0700

    Added 2 html files for the quiz section and the leaderboard section, and set up the HTML skeleton for the quiz portion.

--- script.js
+++ script.js
@@ -4,3 +4,20 @@
 // If wrong answer is selected, deduct time from timer, do not add point, and go to next question
 // Repeat this until question number equals the total length of the questions
 // Report a score and store it in the leaderboards
+
+// Start button, when clicked triggers start game function
+const startBtn = document.getElementById('start')
+
+startBtn.addEventListener("click", startGame)
+
+// function startGame {
+
+// }
+
+// function nextQuestion {
+
+// }
+
+// function answer {
+
+// }
